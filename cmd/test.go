c228e01be782c8164f5f8b2ec03fb145f15078cb
Robert Sandoval <rsandova@redhat.com>
2020-06-04 10:20:30 -0500

    Updated

---
+++ cmd/test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"log"
+)
+
+type HelpMe struct {
+	Clustername string `yaml:"clustername"`
+	StaticIP    bool   `yaml:"staticips"`
+	Domain      string `yaml:"domain"`
+	DNSService  DNS    `yaml:"dns"`
+}
+type DNS struct {
+	ClusterID  string      `yaml:"clusterid"`
+	Domain     string      `yaml:"domain"`
+	Forwarders []Forwarder `yaml:"forwarders"`
+}
+type Forwarder struct {
+	Forwarder string `yaml:"forwarder"`
+} /*type Node struct {
+
+}*/
+
+var helpme HelpMe
+
+func main() {
+	yamlFile, err := ioutil.ReadFile("../helper.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &helpme)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	fmt.Println("DNS: " + helpme.DNSService.Domain + "\n")
+	printStatus()
+
+}
+
+func printStatus() {
+	fmt.Println("clustername: " + helpme.Clustername)
+	fmt.Println("domain: " + helpme.DNSService.Domain)
+	fmt.Printf("staticips: %v\n", helpme.StaticIP)
+
+	for _, f := range helpme.DNSService.Forwarders {
+		fmt.Println("forwarder: " + f.Forwarder)
+	}
+
+}
---
+++ dns_type.go
@@ -0,0 +1,16 @@
+package main
+
+type helpme struct {
+	domain      string `yaml:"domain"`
+	clusterName string `yaml:"domain"`
+	forwarder1  string `yaml:"domain,omitempty"`
+	forwarder2  string `yaml:"domain,omitempty"`
+}
+
+/*func CreateDNS() dns {
+    dns:=DNS{}
+    dns.fowarder1 = "8.8.8.8"
+    dns.fowarder2 = "8.8.4.4"
+
+    return dns
+}*/

80685c42cebc30f433d426a3c3a667faa9834e07
Robert Sandoval <rsandova@redhat.com>
2020-07-02 13:05:48 -0500

    Updated

--- test.go
+++ test.go
@@ -5,29 +5,32 @@ import (
 	"fmt"
 	buildah "github.com/containers/buildah"
 	"github.com/containers/storage"
+	"os"
 	//	"time"
 )
 
 func main() {
 	image := "docker.io/library/tomcat"
-
 	storeOptions, err := storage.DefaultStoreOptionsAutoDetectUID()
+	store, err := storage.GetStore(storeOptions)
+
+	options := buildah.PullOptions{
+		Store:        store,
+		ReportWriter: os.Stderr,
+	}
 	if err != nil {
 		fmt.Println(err)
 	}
-	store, err := storage.GetStore(storeOptions)
+	fmt.Println(store)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	poptions := buildah.PullOptions{
-		Store: store,
-	}
 	//	ctx, err := context.WithTimeout(context.Background(), 600*time.Second)
 	//	if err != nil {
 	//		fmt.Println(err)
 	//	}
-	id, err := buildah.Pull(context.TODO(), image, poptions)
+	id, err := buildah.Pull(context.TODO(), image, options)
 
 	fmt.Println("here")
 	if err != nil {
